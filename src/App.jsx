3df254c0daaf4ebc31506883fe511a0043789bb9
Leonel Cordero <leonelrcordero22@gmail.com>
2024-01-03 15:51:57 -0300

    Componentes creados

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Titulo from "./Titulo";
+
+function App() {
+  return (
+  <>
+    <Titulo/>
+  </>
+  )
+}
+
+export default App;
---
+++ src/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <div className='mt-5'>
+            <h1 className='d-flex justify-content-center'>Hello world!</h1>
+        </div>
+    );
+};
+
+export default Titulo;
